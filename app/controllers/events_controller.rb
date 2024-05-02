1db238c9b4e5e8986f5bf5c10cb204acd75cf898
SyedSibtainRazvi <syedsibtain191@gmail.com>
2024-05-02 23:53:49 +0530

    feat: added events route for movies

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,10 @@
+# Here EventsController is a controller class that inherits from ApplicationController.
+# This controller class will be responsible for handling requests related to events.
+
+class EventsController < ApplicationController
+
+
+    def index
+        
+    end
+end 
